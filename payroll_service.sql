c697fb026fdb6781597c58baf05b7ebb30ecb78d
sunnyok03 <sunnyok03@gmail.com>
2023-12-10 19:32:03 +0530

    UC2 added a table employee_payroll

--- payroll_service.sql
+++ payroll_service.sql
@@ -3,4 +3,12 @@ create database payroll_service;
 use payroll_service;
 show databases;
 
+# USE CASE 2
+create table employee_payroll(
+	column_id int auto_increment,
+    name varchar(100),
+    salary double,
+    start_date date,
+    primary key (column_id)
+);
 
