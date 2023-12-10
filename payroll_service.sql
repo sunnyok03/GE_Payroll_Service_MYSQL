dddd87880097cf806509a8f050ac1e1ca202da8b
sunnyok03 <sunnyok03@gmail.com>
2023-12-10 19:48:53 +0530

    UC3 added rows to employee_payroll table

--- payroll_service.sql
+++ payroll_service.sql
@@ -12,3 +12,12 @@ create table employee_payroll(
     primary key (column_id)
 );
 
+# USE CASE 3
+insert into employee_payroll (name,salary,start_date) values
+("A",1000,'2024-12-09'),
+("B",2000,"2023-12-10"),
+("C",3000,"2023-12-11");
+
+
+
+
