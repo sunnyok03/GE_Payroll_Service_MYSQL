c40c2e0787c45bb4c85d239fe088cd6b2238eec3
sunnyok03 <sunnyok03@gmail.com>
2023-12-11 22:20:07 +0530

    UC10 improved entity relations to prevent multivalues

--- payroll_service.sql
+++ payroll_service.sql
@@ -50,3 +50,36 @@ alter table employee_payroll add basic_pay double, add deduction double, add tax
 insert into employee_payroll (gender,name,salary,start_date,phone,address,department,basic_pay,deduction,taxable_pay,income_tax,net_pay) values ('F',"Terissa",50000,"2023-12-11",9191919191,"Hyd","sale",30000,10000,30000,5000,25000);
 insert into employee_payroll (gender,name,salary,start_date,phone,address,department,basic_pay,deduction,taxable_pay,income_tax,net_pay) values ('F',"Terissa",50000,"2023-12-11",9191919191,"Hyd","marketing",30000,10000,30000,5000,25000);
 
+# USE CASE 10 (ER Diagram)
+create table Company(
+	comp_id int primary key,
+    comp_name varchar(100)
+);
+
+insert into Company (comp_id,comp_name) values (1,"Company 1");
+
+create table Department(
+	dept_id int primary key,
+    dept_name varchar(100)
+);
+
+insert into Department(dept_id,dept_name) values (1,"Sales");
+insert into Department(dept_id,dept_name) values (2,"Marketing");
+
+create table Employee(
+    EmployeeID int primary key auto_increment,
+    EmployeeName varchar(100),
+    PhoneNumber varchar(20),
+    Address varchar(200),
+    Gender char(1),
+    StartDate date,
+    dept_id int,
+    comp_id int,
+    FOREIGN KEY (dept_id) REFERENCES Department(dept_id),
+    FOREIGN KEY (comp_id) REFERENCES Company(comp_id)
+);
+
+insert into Employee values (1,"Terissa",9191919191,"Hyd",'F',"2023-12-11",1,1);
+
+
+
