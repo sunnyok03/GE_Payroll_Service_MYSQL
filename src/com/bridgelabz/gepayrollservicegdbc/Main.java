cc210bfe3011f1a1d575677914ba5afafb73012c
sunnyok03 <sunnyok03@gmail.com>
2023-12-12 07:26:02 +0530

    JDBC3And4 implement updateBasicPay method

--- src/com/bridgelabz/gepayrollservicegdbc/Main.java
+++ src/com/bridgelabz/gepayrollservicegdbc/Main.java
@@ -4,6 +4,9 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+Queries to implement CRUD on employee_payroll table
+ */
 public class Main {
     private static final String URL = "jdbc:mysql://localhost:3306/payroll_service";
     private static final String USERNAME = "root";
@@ -12,19 +15,27 @@ public class Main {
     public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
             System.out.println("Connection established...");
-            List<EmployeePayroll> list = retrieveData(connection);
-            for(EmployeePayroll employee : list){
-                System.out.println(employee);
-            }
+//            List<EmployeePayroll> list = retrieveData();
+//            for(EmployeePayroll employee : list){
+//                System.out.println(employee);
+//            }
+
+            // update basePay of "Terissa" to 3000000.00 and sync with db
+            updateBasicPay(3000000.00,"Terissa");
         }catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    private static List<EmployeePayroll> retrieveData(Connection connection){
+    /*
+    @desc: retrieve data from the table employee_payroll
+    @params: connection
+    @return: list of data
+     */
+    private static List<EmployeePayroll> retrieveData(){
         List<EmployeePayroll> list = new ArrayList<>();
 
-        try(connection){
+        try(Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
             String query = "select * from employee_payroll";
             PreparedStatement preparedStatement = connection.prepareStatement(query);
             ResultSet resultSet = preparedStatement.executeQuery();
@@ -47,5 +58,28 @@ public class Main {
             throw new RuntimeException(e);
         }
         return list;
+    }
+
+
+    /*
+    @desc: update basicPay with given name
+    @params: connection,basicPay,name
+     */
+    private static void updateBasicPay(double basicPay, String name){
+        try(Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
+            String query = "update employee_payroll set basic_pay = ? where name = ?";
+            System.out.println(query);
+            try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+                preparedStatement.setDouble(1,basicPay);
+                preparedStatement.setString(2,name);
+
+                if(preparedStatement.executeUpdate() > 0){
+                    System.out.println("DB updated with new basicPay...");
+                }
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
     }
 }
