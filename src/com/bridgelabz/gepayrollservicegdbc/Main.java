372673a158efe689ef5911e7d99f5d1bd053a406
sunnyok03 <sunnyok03@gmail.com>
2023-12-12 07:54:30 +0530

    JDBC5 retrieve joinees between start and end date

--- src/com/bridgelabz/gepayrollservicegdbc/Main.java
+++ src/com/bridgelabz/gepayrollservicegdbc/Main.java
@@ -1,6 +1,7 @@
 package com.bridgelabz.gepayrollservicegdbc;
 
 import java.sql.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,13 +16,19 @@ public class Main {
     public static void main(String[] args) {
         try(Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
             System.out.println("Connection established...");
-//            List<EmployeePayroll> list = retrieveData();
+            List<EmployeePayroll> list = retrieveData();
 //            for(EmployeePayroll employee : list){
 //                System.out.println(employee);
 //            }
 
             // update basePay of "Terissa" to 3000000.00 and sync with db
-            updateBasicPay(3000000.00,"Terissa");
+//            updateBasicPay(3000000.00,"Terissa");
+
+            // fetch data of joinee between startDate and endDate
+           list = joinedBetweenDates(LocalDate.of(2023,12,10),LocalDate.of(2023,12,11));
+                    for(EmployeePayroll employeePayroll : list){
+                        System.out.println(employeePayroll.getName());
+                    }
         }catch (SQLException e) {
             throw new RuntimeException(e);
         }
@@ -60,7 +67,6 @@ public class Main {
         return list;
     }
 
-
     /*
     @desc: update basicPay with given name
     @params: connection,basicPay,name
@@ -81,5 +87,42 @@ public class Main {
         }catch (SQLException e){
             throw new RuntimeException(e);
         }
+    }
+
+    /*
+    @desc: find all employees joined in particular date range
+    @params: start date and end date
+    @return : list of employees
+     */
+    private static List<EmployeePayroll> joinedBetweenDates(LocalDate startDate,LocalDate endDate){
+        List<EmployeePayroll> list = new ArrayList<>();
+
+        try(Connection connection = DriverManager.getConnection(URL,USERNAME,PASSWORD)){
+            String query = "select * from employee_payroll where start_date between ? and ?";
+            System.out.println(query);
+            try(PreparedStatement preparedStatement = connection.prepareStatement(query)){
+                preparedStatement.setDate(1,Date.valueOf(startDate));
+                preparedStatement.setDate(2,Date.valueOf(endDate));
+                ResultSet resultSet = preparedStatement.executeQuery();
+
+                while(resultSet.next()) {
+                    EmployeePayroll employeePayroll = new EmployeePayroll();
+                    employeePayroll.setEmpId(resultSet.getInt("column_id"));
+                    employeePayroll.setGender(resultSet.getString("gender").charAt(0));
+                    employeePayroll.setName(resultSet.getString("name"));
+                    employeePayroll.setSalary(resultSet.getDouble("salary"));
+                    employeePayroll.setStartDate(resultSet.getDate("start_date").toLocalDate());
+                    employeePayroll.setPhoneNumber(resultSet.getString("phone"));
+                    employeePayroll.setAddress(resultSet.getString("address"));
+                    employeePayroll.setBasicPay(resultSet.getDouble("basic_pay"));
+                    employeePayroll.setDept(resultSet.getString("department"));
+                    list.add(employeePayroll);
+                }
+            }
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+        return list;
     }
 }
